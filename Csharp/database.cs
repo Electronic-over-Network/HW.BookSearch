b665713cad0fb770220724280239a062b9e92833
GKO95 <44629526+GKO95@users.noreply.github.com>
2021-03-07 00:54:10 +0900

    Update C# project

--- Csharp/database.cs
+++ Csharp/database.cs
@@ -6,34 +6,90 @@ namespace HW.BookSearch
 {
     //==================================================
     // 데이터베이스 클래스
-    // : 책 정보를 넘파이 형태가 아닌 정돈된 클래스로 정리한다.
+    // : UI에서 요청한 정보를 제공하거나 입력받아 업데이트한다.
     //==================================================
     class Database
     {
         private readonly List<Book> booklist = new();
+        private readonly string filepath;
 
         /* 초기화: 자료 불러오기
-        텍스트 파일에 있는 책 목록을 넘파이로 불러와 Book 클래스로 구성된 리스트로 정리하여
-        Database 객체에 저장한다.
-
-        여기서 'UTF-8'은 텍스트가 8비트 유니코드로 인코딩되어 있기 때문이다.만일 텍스트가 다른
-        인코딩을 가지는 경우, 그에 맞게 encoding 인자도 변경해야 한다. */
+        텍스트 파일에 있는 책 목록을 한 줄마다 불러와 Book 구조체로 구성된 리스트로 정리하여
+        Database 객체에 저장한다. */
         public Database(string filepath)
         {
-            string[] database = File.ReadAllLines(filepath);
+            this.filepath = filepath;
+            string[] database = File.ReadAllLines(this.filepath);
             foreach (string data in database)
             {
                 // 첫 번째 줄에 출판사와 장르 사이에 띄어쓰기가 두 번 적용되어 있어, StringSplitOptions으로 처리한다.
                 string[] parsed = data.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
 
                 booklist.Add(new Book() {
-                    title = parsed[0],
-                    author = parsed[1],
-                    date = Convert.ToInt32(parsed[2]),
+                    title     = parsed[0],
+                    author    = parsed[1],
+                    date      = parsed[2],
                     publisher = parsed[3],
-                    genre = parsed[4] });
+                    genre     = parsed[4] }
+                );
             }
         }
+
+        public void add(Book book)
+        {
+            booklist.Add(book);
+        }
+
+        public void search(int field, string value)
+        {
+            foreach (var book in booklist)
+            {
+                switch ((BKFIELD)field)
+                {
+                    case BKFIELD.TITLE:
+                        if (book.title == value) show(booklist.IndexOf(book));
+                        break;
+                    case BKFIELD.AUTHOR:
+                        if (book.author == value) show(booklist.IndexOf(book));
+                        break;
+                    case BKFIELD.DATE:
+                        if (book.date == value) show(booklist.IndexOf(book));
+                        break;
+                    case BKFIELD.PUBLISHER:
+                        if (book.publisher == value) show(booklist.IndexOf(book));
+                        break;
+                    case BKFIELD.GENRE:
+                        if (book.genre == value) show(booklist.IndexOf(book));
+                        break;
+                }
+            }
+        }
+
+        public void edit(int index, int field, string value)
+        { 
+
+        }
+
+        public void remove(int index)
+        {
+            booklist.RemoveAt(index);
+        }
+
+        public void show(int index)
+        {
+            Console.WriteLine($"{index}. {booklist[index].title} ({booklist[index].date})");
+            Console.WriteLine($"  * Author: {booklist[index].author}");
+            Console.WriteLine($"  * Genre: {booklist[index].genre}");
+            Console.WriteLine($"  * Publisher: {booklist[index].publisher}");
+        }
+
+        public void save()
+        { 
+        
+        }
+
+        // 데이터베이스 책 개수
+        public int Length { get { return booklist.Count; } }
     }
 
     //==================================================
@@ -44,8 +100,17 @@ namespace HW.BookSearch
     {
         public string title;
         public string author;
-        public int date;
+        public string date;
         public string publisher;
         public string genre;
+    }
+
+    enum BKFIELD
+    { 
+        TITLE,
+        AUTHOR,
+        DATE,
+        PUBLISHER,
+        GENRE,
     }
 }
--- Csharp/main.cs
+++ Csharp/main.cs
@@ -5,10 +5,9 @@ namespace HW.BookSearch
     class Program
     {
         /* 시작점
-        본격적으로 파이썬이 시작되는 부분이다.이는 C/C++의 main() 함수
-        역할을 하는 동시, import와 같이 모듈로 호출되면 동작하지 않는다. */
+        프로그램이 시작되는 함수이다. */
         static void Main()
-        {
+        {   
             var _ = new Application("booklist.txt");
         }
     }
@@ -16,12 +15,10 @@ namespace HW.BookSearch
     class Application
     {
         private readonly Database database;
-        private readonly string filepath;
 
         public Application(string filepath)
         {
-            this.filepath = filepath;
-            database = new(this.filepath);
+            database = new(filepath);
             SetupUI();
         }
 
@@ -30,16 +27,93 @@ namespace HW.BookSearch
         ///</summary>
         private void SetupUI()
         {
+            while (true)
+            {
+                Console.WriteLine("=====================");
+                Console.WriteLine("|      OPTIONS      |");
+                Console.WriteLine("| ----------------- |");
+                Console.WriteLine("| 1. Add            |");
+                Console.WriteLine("| 2. Search         |");
+                Console.WriteLine("| 3. Edit           |");
+                Console.WriteLine("| 4. Remove         |");
+                Console.WriteLine("| 5. List           |");
+                Console.WriteLine("| 6. Save           |");
+                Console.WriteLine("| 7. Exit           |");
+                Console.WriteLine("=====================");
+                Console.Write("Select from the menu:");
 
+                int option;
+                try
+                {
+                    option = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid input.\n");
+                    continue;
+                }
+
+                switch (option)
+                {
+                    case 1:
+                        add();
+                        break;
+                    case 2:
+                        search();
+                        break;
+                    case 3:
+                        edit();
+                        break;
+                    case 4:
+                        remove();
+                        break;
+                    case 5:
+                        show();
+                        break;
+                    case 6:
+                        save();
+                        break;
+                    case 7:
+                        save();
+                        return;
+                    default:
+                        Console.WriteLine("Invalid input.");
+                        break;
+                }
+                Console.WriteLine();
+            }
         }
-
 
         ///<summary>
         ///사용자 인터페이스: 1. 도서 추가
         ///</summary>
         private void add()
         {
+            Console.Write(">> Enter the title: ");
+            string title = Console.ReadLine();
 
+            Console.Write(">> Enter the author: ");
+            string author = Console.ReadLine();
+
+            Console.Write(">> Enter the published date: ");
+            string date = Console.ReadLine();
+
+            Console.Write(">> Enter the publisher: ");
+            string publisher = Console.ReadLine();
+
+            Console.Write(">> Enter the genre: ");
+            string genre = Console.ReadLine();
+
+            database.add(new Book()
+            {
+                title = title,
+                author = author,
+                date = date,
+                publisher = publisher,
+                genre = genre
+            });
+
+            Console.WriteLine("Book added!");
         }
 
         ///<summary>
@@ -47,7 +121,26 @@ namespace HW.BookSearch
         ///</summary>
         private void search()
         {
+            Console.WriteLine($"  {(int)BKFIELD.TITLE}. Title");
+            Console.WriteLine($"  {(int)BKFIELD.AUTHOR}. Author");
+            Console.WriteLine($"  {(int)BKFIELD.DATE}. Published date");
+            Console.WriteLine($"  {(int)BKFIELD.PUBLISHER}. Publisher");
+            Console.WriteLine($"  {(int)BKFIELD.GENRE}. Genre");
 
+            try
+            {
+                Console.Write(">> Select the book field to search: ");
+                int field = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write(">> Enter the book field content to search:");
+                string value = Console.ReadLine();
+
+                database.search(field, value);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input.");
+            }
         }
 
         ///<summary>
@@ -63,7 +156,27 @@ namespace HW.BookSearch
         ///</summary>
         private void remove()
         {
+            show();
 
+            int index;
+            Console.Write(">> Select the book to remove: ");
+            try
+            {
+                index = Convert.ToInt32(Console.ReadLine());
+                if (index < database.Length && !(index < 0))
+                {
+                    database.remove(index);
+                    Console.WriteLine("Book removed!");
+                }
+                else
+                {
+                    Console.WriteLine("Unable to find a book from the index.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input.");
+            }
         }
 
         ///<summary>
@@ -71,7 +184,10 @@ namespace HW.BookSearch
         ///</summary>
         private void show()
         {
-
+            for (int index = 0; index < database.Length; index++)
+            {
+                database.show(index);
+            }
         }
 
         ///<summary>
